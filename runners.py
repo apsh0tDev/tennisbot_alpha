b0595034a35f3466beaa71d3717ceeb01af4d0af
apsh0t <154575166+apsh0tDev@users.noreply.github.com>
2024-07-05 02:55:06 -0400

    Update runners.py

--- runners.py
+++ runners.py
@@ -67,7 +67,7 @@ def check_hour():
             logger.info("Removing live data job")
             scheduler.remove_job(get_live_data)
 
-    if ny_time.hour == 2:
+    if ny_time.hour == 3:
         print("this is just a test")
     else:
         logger.info("It's not time to do anything")
@@ -94,7 +94,7 @@ job_defaults = {
 scheduler.configure(job_defaults=job_defaults)
 #scheduler.add_job(all_runners, 'interval', minutes=1)
 scheduler.add_job(get_schedule, 'interval', minutes=30)
-scheduler.add_job(check_hour, 'interval', minutes=30 )
+scheduler.add_job(check_hour, 'interval', minutes=1 )
 scheduler.add_job
 
 
